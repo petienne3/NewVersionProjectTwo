a619fe14e65ae3064288046349572b455b36c1fd
sarmand19 <sarmand19@gmail.com>
2019-05-22 01:51:44 -0400

    Changed Providers table to a one to many relation with benefit plan table

--- src/main/java/com/revature/entities/Providers.java
+++ src/main/java/com/revature/entities/Providers.java
@@ -2,6 +2,7 @@ package com.revature.entities;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -11,21 +12,40 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="Providers")
 public class Providers {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "providerId")
 	private int providerId;
 	
-	private String providerName;
-	
+	@Column(name="description")
 	private String description;
 	
+	@Column(name = "name", nullable = false)
+	private String name;
+	
+//	@ManyToMany(fetch = FetchType.EAGER)
+//	@JoinColumn(name = "companyId")
+//	public List<Company> company;
+	
+//	@ManyToMany(fetch = FetchType.LAZY)
+//	@JoinColumn(name = "BenefitId")
+	
+	
+	@OneToMany(fetch=FetchType.LAZY, cascade= {CascadeType.MERGE, CascadeType.DETACH}) // @OneToMany - dictates that this relationship is one-to-many
+	@JoinColumn(name = "provider_id")
+	public List<BenefitPlan>benefitPlan;
+	
+	@Column (name="status")
 	private int status;
-	
-	private int cost;
-	
-	private int benefitId;
+
+	@Column (name="cost")
+	private double cost;
 
 	/**
 	 * @return the providerId
@@ -42,20 +62,6 @@ public class Providers {
 	}
 
 	/**
-	 * @return the providerName
-	 */
-	public String getProviderName() {
-		return providerName;
-	}
-
-	/**
-	 * @param providerName the providerName to set
-	 */
-	public void setProviderName(String providerName) {
-		this.providerName = providerName;
-	}
-
-	/**
 	 * @return the description
 	 */
 	public String getDescription() {
@@ -67,6 +73,34 @@ public class Providers {
 	 */
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the benefitPlan
+	 */
+	public List<BenefitPlan> getBenefitPlan() {
+		return benefitPlan;
+	}
+
+	/**
+	 * @param benefitPlan the benefitPlan to set
+	 */
+	public void setBenefitPlan(List<BenefitPlan> benefitPlan) {
+		this.benefitPlan = benefitPlan;
 	}
 
 	/**
@@ -86,40 +120,28 @@ public class Providers {
 	/**
 	 * @return the cost
 	 */
-	public int getCost() {
+	public double getCost() {
 		return cost;
 	}
 
 	/**
 	 * @param cost the cost to set
 	 */
-	public void setCost(int cost) {
+	public void setCost(double cost) {
 		this.cost = cost;
-	}
-
-	/**
-	 * @return the benefitId
-	 */
-	public int getBenefitId() {
-		return benefitId;
-	}
-
-	/**
-	 * @param benefitId the benefitId to set
-	 */
-	public void setBenefitId(int benefitId) {
-		this.benefitId = benefitId;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + benefitId;
-		result = prime * result + cost;
+		result = prime * result + ((benefitPlan == null) ? 0 : benefitPlan.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(cost);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
 		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		result = prime * result + providerId;
-		result = prime * result + ((providerName == null) ? 0 : providerName.hashCode());
 		result = prime * result + status;
 		return result;
 	}
@@ -133,21 +155,24 @@ public class Providers {
 		if (getClass() != obj.getClass())
 			return false;
 		Providers other = (Providers) obj;
-		if (benefitId != other.benefitId)
+		if (benefitPlan == null) {
+			if (other.benefitPlan != null)
+				return false;
+		} else if (!benefitPlan.equals(other.benefitPlan))
 			return false;
-		if (cost != other.cost)
+		if (Double.doubleToLongBits(cost) != Double.doubleToLongBits(other.cost))
 			return false;
 		if (description == null) {
 			if (other.description != null)
 				return false;
 		} else if (!description.equals(other.description))
 			return false;
-		if (providerId != other.providerId)
-			return false;
-		if (providerName == null) {
-			if (other.providerName != null)
+		if (name == null) {
+			if (other.name != null)
 				return false;
-		} else if (!providerName.equals(other.providerName))
+		} else if (!name.equals(other.name))
+			return false;
+		if (providerId != other.providerId)
 			return false;
 		if (status != other.status)
 			return false;
@@ -156,9 +181,29 @@ public class Providers {
 
 	@Override
 	public String toString() {
-		return "Providers [providerId=" + providerId + ", providerName=" + providerName + ", description=" + description
-				+ ", status=" + status + ", cost=" + cost + ", benefitId=" + benefitId + "]";
+		return "Providers [providerId=" + providerId + ", description=" + description + ", name=" + name
+				+ ", benefitPlan=" + benefitPlan + ", status=" + status + ", cost=" + cost + "]";
 	}
+
+	public Providers(int providerId, String description, String name, List<BenefitPlan> benefitPlan, int status,
+			double cost) {
+		super();
+		this.providerId = providerId;
+		this.description = description;
+		this.name = name;
+		this.benefitPlan = benefitPlan;
+		this.status = status;
+		this.cost = cost;
+	}
+
+	public Providers() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}//end class providers
+
 	
 	
 	
@@ -168,4 +213,10 @@ public class Providers {
 
 	
 	
-}
+	
+	
+	
+
+	
+	
+
